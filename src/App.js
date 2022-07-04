116399e7435d35b58f30fd55d3930eebca3e389b
Anumol K Mani <anumol.km32@gmail.com>
2022-07-04 16:16:59 +0400

    initail commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import {
+  FaEnvelopeOpen,
+  FaUser,
+  FaCalendarTimes,
+  FaMap,
+  FaPhone,
+  FaLock,
+} from "react-icons/fa";
+const url = "https://randomuser.me/api/";
+const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [person, setPerson] = useState(null);
+  const [title, setTitle] = useState("name");
+  const [value, setValue] = useState("random person");
+  const hanleValue = (e) => {
+    console.log(e.target)
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="block bcg-black"> </div>
+      <div className="block">
+        <div className="container">
+          <img
+            src={(person && person.image) || defaultImage}
+            alt="random user"
+            className="user-img"
+          />
+          <p className="user-title">My {title} is </p>
+          <p className="user-value">{value}</p>
+          <div className="values-list">
+            <button className="icon" data-label="name" onMouseOver={hanleValue}>
+              <FaUser />
+            </button>
+           
+            <button className="icon" data-label="email" onMouseOver={hanleValue}>
+              <FaEnvelopeOpen />
+            </button>
+            <button className="icon" data-label="age" onMouseOver={hanleValue}>
+              < FaCalendarTimes />
+            </button>
+            <button className="icon" data-label="street" onMouseOver={hanleValue}>
+              < FaMap />
+            </button>
+            <button className="icon" data-label="phone" onMouseOver={hanleValue}>
+              < FaPhone />
+            </button>
+            <button className="icon" data-label="password" onMouseOver={hanleValue}>
+              <  FaLock />
+            </button>
+          </div>
+         <button className="btn" type="button">
+          {loading?"Loading...":'random person'}
+         </button>
+        </div>
+      </div>
+    </main>
   );
 }
 
